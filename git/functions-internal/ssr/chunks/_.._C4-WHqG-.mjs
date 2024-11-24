e211710a6bf432b79d3b76ffc2d91dc5deccf4a4
Ander Mancisidor Pikabea <andermancisidorpikabea@MacBook-Pro-de-Ander-2.local>
2024-11-24 13:16:46 +0100

    comentar todo lo relacionado con bd

--- git/functions-internal/ssr/chunks/_.._C4-WHqG-.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/__C-a720sb.mjs');
-
-export { page };
--- git/functions-internal/ssr/chunks/index_YI6PZBE3.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_Iuo0HWsJ.mjs').then(n => n.i);
-
-export { page };
